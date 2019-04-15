3eea7d1ee140df588cee9d36e26c895f4f35fae7
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-15 14:48:01 +0400

    Add forum service layer

---
+++ ForumApp.Data/IForum.cs
@@ -0,0 +1,18 @@
+﻿using ForumApp.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ForumApp.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int forumId);
+        Task UpdateForumTitle(int forumId, string newTitle);
+        Task UpdateForumDescription(int forumId, string newDescription);
+    }
+}
---
+++ ForumApp.Service/ForumService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ForumApp.Data;
+using ForumApp.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApp.Service
+{
+    public class ForumServices : IForum
+    {
+        private readonly ApplicationDbContext _context;
+        public ForumServices(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums.Include(forum => forum.Posts).AsNoTracking(); ;
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
