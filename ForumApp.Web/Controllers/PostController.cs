f3e4206a16e5cefb69d63392c7dd4a58d9a0f0a1
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-15 23:58:53 +0400

    Add NewPostModel , posts create action

--- ForumApp.Web/Controllers/PostController.cs
+++ ForumApp.Web/Controllers/PostController.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using ForumApp.Data;
+﻿using ForumApp.Data;
 using ForumApp.Data.Models;
 using ForumApp.Web.Models.Post;
 using ForumApp.Web.Models.Reply;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ForumApp.Web.Controllers
 {
     public class PostController : Controller
     {
-        private readonly IPost _postService; 
-        public PostController(IPost postService)
+        private readonly IPost _postService;
+        private readonly IForum _forumService;
+        private static UserManager<ApplicationUser> _userManager;
+
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
         public IActionResult Index(int id)
         {
@@ -26,7 +33,7 @@ namespace ForumApp.Web.Controllers
             {
                 Id = post.Id,
                 AuthorImageUrl = post.User.ProfileImageUrl,
-                AuthorName=post.User.UserName,
+                AuthorName = post.User.UserName,
                 AuthorId = post.User.Id,
                 AuthorRating = post.User.Rating,
                 Created = post.Created,
@@ -35,6 +42,36 @@ namespace ForumApp.Web.Controllers
                 Replies = replies
             };
             return View(model);
+        }
+
+        public IActionResult Create(int id)
+        {
+            //id is Forum.Id
+            var forum = _forumService.GetById(id);
+
+            var model = new NewPostModel
+            {
+                ForumId = forum.Id,
+                ForumTitle = forum.Title,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPostAsync(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var post = BuildPost(model, user);
+        }
+
+        private object BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            throw new NotImplementedException();
         }
 
         private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
---
+++ ForumApp.Web/Models/Post/NewPostModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumApp.Web.Models.Post
+{
+    public class NewPostModel
+    {
+        public string ForumTitle { get; set; }
+        public int ForumId { get; set; }
+        public string AuthorName { get; set; }
+        public string ForumImageUrl { get; set; }
+    }
+}
