3e6b611c661bbe340f7ec44a39de4b58f3a53285
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-17 15:32:13 +0400

    Add HomeIndexModel , home view and home controller actions

--- ForumApp.Web/Controllers/HomeController.cs
+++ ForumApp.Web/Controllers/HomeController.cs
@@ -1,37 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using ForumApp.Web.Models.Forum;
+using ForumApp.Web.Models.Home;
+using ForumApp.Web.Models.Post;
 using Microsoft.AspNetCore.Mvc;
-using ForumApp.Web.Models;
+using System.Linq;
 
 namespace ForumApp.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPost _postService;
+        public HomeController(IPost postService)
+        {
+            _postService = postService;
+        }
         public IActionResult Index()
         {
-            return View();
+            var model = BuildIndexModel();
+            return View(model);
         }
 
-        public IActionResult About()
+        private HomeIndexModel BuildIndexModel()
         {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
+            var latest = _postService.GetLatestPosts(10);
+            var posts = latest.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorName = post.User.UserName,
+                AuthorId = post.User.Id,
+                AuthorRating = post.User.Rating,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = _postService.GetReplyCount(post.Id),
+                Forum = BuildForumListing(post.Forum)
+            });
+            return new HomeIndexModel
+            {
+                LatestPosts = posts
+            };
         }
 
-        public IActionResult Contact()
+        private ForumListingModel BuildForumListing(Forum forum)
         {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
+            return new ForumListingModel
+            {
+                Title = forum.Title,
+                Id = forum.Id,
+                Description = forum.Description,
+                ImageUrl = forum.ImageUrl
+            };
         }
 
-        public IActionResult Error()
+        [HttpPost]
+        public IActionResult Search(string searchQuery)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return RedirectToAction("Topic", "Forum", new { searchQuery });
         }
     }
 }
---
+++ ForumApp.Web/Models/Home/HomeIndexModel.cs
@@ -0,0 +1,14 @@
+﻿using ForumApp.Web.Models.Post;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumApp.Web.Models.Home
+{
+    public class HomeIndexModel
+    {
+        public string SearchQuery { get; set; }
+        public IEnumerable<PostListingModel> LatestPosts { get; set; }
+    }
+}
