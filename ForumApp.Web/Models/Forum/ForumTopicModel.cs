f77434e4d00f5c07bc30c275edafed884bdbbaf8
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-19 15:19:27 +0400

    Add forums topic view, property for search query

--- ForumApp.Web/Models/Forum/ForumTopicModel.cs
+++ ForumApp.Web/Models/Forum/ForumTopicModel.cs
@@ -8,5 +8,6 @@ namespace ForumApp.Web.Models.Forum
     {
         public ForumListingModel Forum { get; set; }
         public IEnumerable<PostListingModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
     }
 }
