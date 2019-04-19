5a3f24d4dc5ebc5b4bcdc941e7085e5b4d3e9dc1
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-20 02:01:57 +0400

    Add search controller and view model

---
+++ ForumApp.Web/Controllers/SearchController.cs
@@ -0,0 +1,70 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using ForumApp.Web.Models.Forum;
+using ForumApp.Web.Models.Post;
+using ForumApp.Web.Models.Search;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace ForumApp.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IPost _postService;
+
+        public SearchController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Result(string searchQuery)
+        {
+            var posts = _postService.GetFilteredPosts(searchQuery);
+
+            var noPostsFound = (!string.IsNullOrEmpty(searchQuery) && !posts.Any());
+
+            var postListingModel = posts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorName = post.User.UserName,
+                AuthorId = post.User.Id,
+                AuthorRating = post.User.Rating,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = _postService.GetReplyCount(post.Id),
+                Forum = BuildForumListing(post.Forum)
+            });
+
+            var model = new SearchResultModel
+            {
+                Posts = postListingModel,
+                SearchQuery = searchQuery,
+                EmptySearchResult = noPostsFound
+            };
+
+            return View(model);
+        }
+
+        private ForumListingModel BuildForumListing(Forum forum)
+        {
+            return new ForumListingModel
+            {
+                Title = forum.Title,
+                Id = forum.Id,
+                Description = forum.Description,
+                ImageUrl = forum.ImageUrl
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Search(string searchQuery)
+        {
+            return RedirectToAction("Results", new { searchQuery });
+        }
+    }
+}
---
+++ ForumApp.Web/Models/Search/SearchResultModel.cs
@@ -0,0 +1,15 @@
+﻿using ForumApp.Web.Models.Post;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumApp.Web.Models.Search
+{
+    public class SearchResultModel
+    {
+        public IEnumerable<PostListingModel> Posts { get; set; }
+        public string SearchQuery { get; set; }
+        public bool EmptySearchResult { get; set; }
+    }
+}
