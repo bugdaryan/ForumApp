144d2e70af595c2c0307ca4cafa393a14756cf60
Spartak Bugdaryan <bugdaryan@gmail.com>
2019-04-20 02:48:26 +0400

    Add applicationUser service layer bump rating not implemented

---
+++ ForumApp.Data/IApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using ForumApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumApp.Data
+{
+    public interface IApplicationUser
+    {
+        ApplicationUser GetById(string id);
+        IEnumerable<ApplicationUser> GetAll();
+
+        Task SetProfileImageAsync(string id, Uri uri);
+        Task IncrementRatingAsync(string id, Type type);
+    }
+}
---
+++ ForumApp.Service/ApplicationUserService.cs
@@ -0,0 +1,41 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumApp.Service
+{
+    public class ApplicationUserService : IApplicationUser
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApplicationUserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<ApplicationUser> GetAll()
+        {
+            return _context.ApplicationUsers;
+        }
+
+        public ApplicationUser GetById(string id)
+        {
+            return GetAll().FirstOrDefault(user => user.Id == id);
+        }
+
+        public Task IncrementRatingAsync(string id, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SetProfileImageAsync(string id, Uri uri)
+        {
+            var user = GetById(id);
+            user.ProfileImageUrl = uri.AbsoluteUri;
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
